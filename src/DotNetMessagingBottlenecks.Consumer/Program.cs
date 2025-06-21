749d75785b25e583fc2bab7973253a9a0a6b5dbf
Nile Kim <nile.coderiver@gmail.com>
2025-06-21 12:27:38 +0900

    add main projects (overwrite original git repo state)

---
+++ src/DotNetMessagingBottlenecks.Consumer/Program.cs
@@ -0,0 +1,7 @@
+using DotNetMessagingBottlenecks.Consumer;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/DotNetMessagingBottlenecks.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNetMessagingBottlenecks.Shared
+{
+    public class Class1
+    {
+
+    }
+}
